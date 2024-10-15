7e942e7ab7a13bb0b59d3ad381a1e977fef64f24
Faver Xavier Neira Molina <fxneiram@gmail.com>
2024-10-14 19:45:33 -0500

    Register

--- src/app/users/register/register.component.ts
+++ src/app/users/register/register.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {DtoUser, UsersService} from '../../core/modules/openapi';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  user: DtoUser = {} as DtoUser;
 
+  constructor(private usersService: UsersService) {}
+
+  onSubmit() {
+    this.usersService.apiV1UsersPost("123456789", this.user).subscribe((data: DtoUser) => {
+      console.log(data);
+    });
+  }
 }
