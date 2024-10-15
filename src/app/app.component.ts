2f0702da42a713dadc07e038c47938af6818410a
Faver Xavier Neira Molina <fxneiram@gmail.com>
2024-10-14 23:04:47 -0500

    base project file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {BASE_PATH, UsersService} from './core/modules/openapi';
-import {HttpClientModule} from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-import { environment } from '../environments/environment';
-import { NavComponent } from './exp/nav/nav.component';
-import {FooterComponent} from './exp/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HttpClientModule, CommonModule, NavComponent, FooterComponent],
-  providers: [UsersService, {provide: BASE_PATH, useValue: environment.apiUrl}],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Demo User Service';
-
-
-  constructor() {}
-
-
+  title = 'demo_angular';
 }
