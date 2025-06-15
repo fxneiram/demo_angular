6ff0cb45cc03573bd05932164c37cb596f5ab8a5
Faver Xavier Neira Molina <fxneiram@gmail.com>
2025-06-14 19:48:54 -0500

    updload component

--- src/app/videos/upload/upload.component.ts
+++ src/app/videos/upload/upload.component.ts
@@ -9,16 +9,41 @@ import { DualLangMediaRequest } from './dto-dual-lang-media-request';
 })
 export class UploadComponent {
   mediaRequest: DualLangMediaRequest = {} as DualLangMediaRequest;
+  file: File | null = null;
   fileName = '';
   fileExtension = '';
   base64file = '';
 
   constructor(private http: HttpClient) { }
 
+  // Método para procesar la carga del archivo
+  onFileChange(event: any): void {
+    const file: File = event.target.files[0];
+    console.log('File selected:', file);
+    if (file) {
+      this.fileName = file.name;
+      this.fileExtension = file.name.split('.').pop() || '';
+      this.file = file;
+
+      // Convertir el archivo a base64
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.base64file = reader.result as string;
+        this.mediaRequest.base64file = this.base64file;  // Asignar el archivo a la solicitud
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  // Método que se llama al enviar el formulario
   onSubmit(): void {
+    if (!this.file) {
+      alert('File is required!');
+      return;
+    }
 
     // Realizar la solicitud HTTP POST
-    this.http.post('https://tu-backend.com/api/upload', this.mediaRequest)
+    this.http.post('http://localhost:8082/api/files/upload', this.mediaRequest)
       .subscribe({
         next: (response) => {
           console.log('Upload successful:', response);
@@ -29,21 +54,5 @@ export class UploadComponent {
           alert('Error uploading file.');
         }
       });
-  }
-
-  // Método para procesar la carga de archivos
-  onFileChange(event: any): void {
-    const file: File = event.target.files[0];
-    if (file) {
-      this.fileName = file.name;
-      this.fileExtension = file.name.split('.').pop() || '';
-
-      // Convertir el archivo a base64
-      const reader = new FileReader();
-      reader.onload = () => {
-        this.base64file = reader.result as string;
-      };
-      reader.readAsDataURL(file);
-    }
   }
 }
